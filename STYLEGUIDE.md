41e917fd63af06499733b7f1361fcabf30bde1a5
john kelly <johnkelly@codehs.com>
2015-07-31 22:49:55 -0700

    add htmlhint, document semantinc html elements, start style guide, normalize css

---
+++ STYLEGUIDE.md
@@ -0,0 +1,19 @@
+# Document to document style decisions.
+
+## HTML
+### Reference
+- view-source:http://learn.shayhowe.com/practice/getting-to-know-html/index.html
+- http://primercss.io/guidelines/#general-formatting
+- https://github.com/yaniswang/HTMLHint/wiki/Rules
+- http://google-styleguide.googlecode.com/svn/trunk/htmlcssguide.xml#Document_Type
+
+### Specifics
+- "tag-self-close": false
+  - Both the google and the github style guide say that it's a bad idea
+
+## JavaScript
+
+TODO - document any more changes that are made here relative to the airbnb.
+
+### Reference
+- https://github.com/airbnb/javascript
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 // Personal API
 const router = require('./router.js');
 
+// Html Routes
 router.register('/$', (req, res) => {
     fs.readFile('./src/index.html', (err, data) => {
         if (err) {
@@ -14,6 +15,25 @@ router.register('/$', (req, res) => {
     });
 });
 
+// Stylesheet Routes
+router.register('/main.css$', (req, res) => {
+    fs.readFile('./src/main.css', (err, data) => {
+        if (err) {
+            res.end('500');
+        }
+        res.end(data);
+    });
+});
+router.register('/normalize.css$', (req, res) => {
+    fs.readFile('./src/normalize.css', (err, data) => {
+        if (err) {
+            res.end('500');
+        }
+        res.end(data);
+    });
+});
+
+// Default Route
 router.register('.', (req, res) => {
     res.end('404');
 });
