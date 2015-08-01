a1881875cf396ac7a1dc02d8bcba24a5faa9b465
john kelly <johnkelly@codehs.com>
2015-07-31 19:46:37 -0700

    jasmine tests for router

--- spec/tests/routerSpec.js
+++ spec/tests/routerSpec.js
@@ -1,15 +1,63 @@
-describe('router', () => {
-    const router = require('../../src/router.js');
+const http = require('http');
 
-    beforeEach(() => {
-        router.register('.', () => {});
+const router = require('../../src/router.js');
+
+// `describe` defines a test suite.
+describe('a router', () => {
+
+    // Setup a server and register some basic routes.
+    beforeAll(() => {
+        http.createServer(router).listen(8080, 'localhost');
+        router.register('/$', (req, res) => {
+            res.end('index');
+        });
+        router.register('.', (req, res) => {
+            res.end('404');
+        });
     });
 
-    it('should be able to blah', () => {
-        // codes.
+    // `it` defines a single test
+    it('can register a route to /$', (done) => {
+        // test does not end until `done` is called
+        http.get('http://localhost:8080/',(res) => {
+            let resData = '';
+            res.on('data',(data) => {
+                resData += data;
+            });
+            res.on('end', () => {
+                expect(resData).toEqual('index');
+                done();
+            })
+        });
     });
 
-    describe('when blah', () => {
-        // codes.
+    it('can register a route to .', (done) => {
+        http.get('http://localhost:8080/asdf',(res) => {
+            let resData = '';
+            res.on('data',(data) => {
+                resData += data;
+            });
+            res.on('end', () => {
+                expect(resData).toEqual('404');
+                done();
+            })
+        });
+    });
+
+    it('can override a registered route', (done) => {
+        router.register('/$', (req, res) => {
+            res.end('index test 2');
+        });
+
+        http.get('http://localhost:8080/',(res) => {
+            let resData = '';
+            res.on('data',(data) => {
+                resData += data;
+            });
+            res.on('end', () => {
+                expect(resData).toEqual('index test 2');
+                done();
+            })
+        });
     });
 });
