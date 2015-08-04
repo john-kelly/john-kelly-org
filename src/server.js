bcacb174f194b03a121524952f73a62c70fc7344
john kelly <johnkelly@codehs.com>
2015-08-03 20:49:15 -0700

    website page layout

--- src/server.js
+++ src/server.js
@@ -32,8 +32,20 @@ router.register('/normalize.css$', (req, res) => {
         res.end(data);
     });
 });
+router.register('/page-layout.css$', (req, res) => {
+    fs.readFile('./src/page-layout.css', (err, data) => {
+        if (err) {
+            res.end('500');
+        }
+        res.end(data);
+    });
+});
 
 // Default Route
+// TODO this needs to be implemented differently.
+// As it currently stands, the order in which a route is tested is not
+// garunteed, therefore, this could theoretically run first, which is not what
+// we want.
 router.register('.', (req, res) => {
     res.end('404');
 });
