7deb75a8be81e54e8cb85fd6d63d3c65af9121ca
john kelly <johnkelly@codehs.com>
2015-08-04 22:15:08 -0700

    add postgres

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {};
+
+config.localDbUrl = 'pg://postgres:5432@localhost/john-kelly-org';
+
+module.exports = config;
--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+
+// 3rd Party
+const pg = require('pg');
 
 // Personal API
+const {localDbUrl} = require('./config.js');
 const router = require('./router.js');
 
 // Html Routes
@@ -47,11 +52,30 @@ router.register('/page-layout.css$', (req, res) => {
     });
 });
 
+// Db Test Route
+router.register('/db$', (req, res) => {
+    pg.connect(process.env.DATABASE_URL || localDbUrl, (err1, client, done) => {
+        if (err1) {
+            res.end(util.format('%j', err1));
+        } else {
+            client.query('SELECT * FROM test_table', (err2, result) => {
+                done();
+                if (err2) {
+                    res.end(util.format('%j', err2));
+                } else {
+                    res.end(util.format('%j', result));
+                }
+            });
+        }
+    });
+});
+
 // Default Route
-// TODO this needs to be implemented differently.
+// FIXME
 // As it currently stands, the order in which a route is tested is not
 // garunteed, therefore, this could theoretically run first, which is not what
-// we want.
+// we want. Default routes should be implemented as an edgecase OR there needs
+// to be a mechanism to determine route priority.
 router.register('.', (req, res) => {
     res.end('404');
 });
