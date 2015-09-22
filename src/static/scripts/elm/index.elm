2ac223ba758ed20f25baae25cc34055f582fe53f
john kelly <johnkelly@codehs.com>
2015-09-21 20:01:47 -0700

    fix npm start, switch out elm code with http example

--- src/static/scripts/elm/index.elm
+++ src/static/scripts/elm/index.elm
@@ -1,29 +1,86 @@
 module Index where
 
-import Html exposing (div, button, text)
-import Html.Events exposing (onClick)
-import StartApp.Simple as StartApp
+import Char
+import Html exposing (..)
+import Html.Attributes as Attr exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Json exposing ((:=))
+import String
+import Task exposing (..)
+
+-- VIEW
+
+view : String -> Result String (List String) -> Html
+view string result =
+  let field =
+        input
+          [ placeholder "Zip Code"
+          , value string
+          , on "input" targetValue (Signal.message query.address)
+          , myStyle
+          ]
+          []
+
+      messages =
+        case result of
+          Err msg ->
+              [ div [ myStyle ] [ text msg ] ]
+
+          Ok cities ->
+              List.map (\city -> div [ myStyle ] [ text city ]) cities
+  in
+      div [] (field :: messages)
 
 
-main =
-  StartApp.start { model = model, view = view, update = update }
-
-
-model = 0
-
-
-view address model =
-  div []
-    [ button [ onClick address Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick address Increment ] [ text "+" ]
+myStyle : Attribute
+myStyle =
+  style
+    [ ("width", "100%")
+    , ("height", "40px")
+    , ("padding", "10px 0")
+    , ("font-size", "2em")
+    , ("text-align", "center")
     ]
 
 
-type Action = Increment | Decrement
+-- WIRING
+
+main =
+  Signal.map2 view query.signal results.signal
 
 
-update action model =
-  case action of
-    Increment -> model + 1
-    Decrement -> model - 1
+query : Signal.Mailbox String
+query =
+  Signal.mailbox ""
+
+
+results : Signal.Mailbox (Result String (List String))
+results =
+  Signal.mailbox (Err "A valid US zip code is 5 numbers.")
+
+
+port requests : Signal (Task x ())
+port requests =
+  Signal.map lookupZipCode query.signal
+    |> Signal.map (\task -> Task.toResult task `andThen` Signal.send results.address)
+
+
+lookupZipCode : String -> Task String (List String)
+lookupZipCode query =
+  let toUrl =
+        if String.length query == 5 && String.all Char.isDigit query
+          then succeed ("http://api.zippopotam.us/us/" ++ query)
+          else fail "Give me a valid US zip code!"
+  in
+      toUrl `andThen` (mapError (always "Not found :(") << Http.get places)
+
+
+places : Json.Decoder (List String)
+places =
+  let place =
+        Json.object2 (\city state -> city ++ ", " ++ state)
+          ("place name" := Json.string)
+          ("state" := Json.string)
+  in
+      "places" := Json.list place
